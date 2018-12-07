48e9d5c480a68c0371dea82129d3d4cc80416e3e
Evgeny Ovchinnikov <e.ovchinnikov@sysolutions.ru>
2018-12-07 17:33:21 +0700

    Created view with several tests

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from blog.models import Entry
 
-# Create your views here.
+
+class HomeView(ListView):
+    template_name = 'index.html'
+    queryset = Entry.objects.order_by('-created_at')
