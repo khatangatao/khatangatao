8ae5d3dafd96a4dac491a5bb5964bc376d09519e
Evgeny Ovchinnikov <e.ovchinnikov@sysolutions.ru>
2018-12-12 16:42:33 +0700

    Create and registration of comment model

--- blog/models.py
+++ blog/models.py
@@ -13,3 +13,15 @@ class Entry(models.Model):
 
     class Meta:
         verbose_name_plural = "entries"
+
+
+class Comment(models.Model):
+    entry = models.ForeignKey(Entry)
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=200)
+    body = models.TextField(max_length=1000)
+    created_at = models.DateTimeField(auto_now_add=True, editable=True)
+    modified_at = models.DateTimeField(auto_now=True, editable=False)
+
+    def __str__(self):
+        return self.body
