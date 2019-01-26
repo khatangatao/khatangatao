ec8a72bd98ff4bf4abed5acf39beab544e982a1c
khatangatao <khatangatao@gmail.com>
2019-01-26 21:56:23 +0700

    Added some test and views

--- blog/views.py
+++ blog/views.py
@@ -14,15 +14,18 @@ class HomeView(ListView):
 
 class EntryDetail(CreateView):
     model = Entry
-    form_class = CommentForm
     template_name = 'blog/entry_detail.html'
+    form_class = CommentForm
 
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
-        kwargs['entry'] = self.model
+        kwargs['entry'] = self.object
         return kwargs
 
     def get_context_data(self, **kwargs):
         d = super().get_context_data(**kwargs)
         d['entry'] = self.get_object()
         return d
+
+    def get_success_url(self):
+        return self.get_object().get_absolute_url()
