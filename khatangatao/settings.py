de973fee976f5c3574b146292cd9b0b1fb374403
Evgeny Ovchinnikov <e.ovchinnikov@sysolutions.ru>
2019-03-06 13:37:37 +0700

    Changes in DEBUG and ALLOWED_HOSTS definition

--- khatangatao/settings.py
+++ khatangatao/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import re
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,9 +24,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_7#@wv#0nk0-^g!+7$l9q!ay&j+k##p6dha5n%^nrl1746bz9h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', 'False').lower() in (
+    "on", "yes", "true", "t", "1"
+)
 
-ALLOWED_HOSTS = []
+if DEBUG:
+    ALLOWED_HOSTS = ['*']
+else:
+    ah = os.environ.get('ALLOWED_HOSTS', '*')
+    ah = re.split(',', ah)
+    ALLOWED_HOSTS = ah
 
 
 # Application definition
@@ -73,16 +81,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'khatangatao.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 
 DATABASES = {
     'default': {
