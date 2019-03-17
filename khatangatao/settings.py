f0206cb81a5e5ae2caf7fc0977ae8a7ac0987141
khatangatao <githubc0m>
2019-03-18 06:48:29 +0700

    Added new static files directory

--- khatangatao/settings.py
+++ khatangatao/settings.py
@@ -139,10 +139,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
 #
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-#     os.path.join(BASE_DIR, 'frontend', 'static'),
-# ]
+STATICFILES_DIRS = [
+    # os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'frontend', 'static'),
+]
 
 
 
