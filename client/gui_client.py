2f9b392173279540b42f4c0e80045f00069b4de1
Evgeny Ovchinnikov <e.ovchinnikov@sysolutions.ru>
2019-04-04 16:26:02 +0700

    Methods fetchRecord, updateRecord rewrited to minimal working state

--- client/gui_client.py
+++ client/gui_client.py
@@ -33,32 +33,69 @@ def makeWidgets():
 
 
 def fetchRecord():
-    key = entries['key'].get()
-    try:
-        record = db[key]                                                # fetch by key, show in GUI
-    except:
-        showerror(title='Error', message='Такой статьи нет!')
-    else:
+    # key = entries['key'].get()
+    # try:
+    #     record = db[key]                                                # fetch by key, show in GUI
+    # except:
+    #     showerror(title='Error', message='Такой статьи нет!')
+    # else:
+    #     for field in fieldnames:
+    #         entries[field].delete(0, END)
+    #         entries[field].insert(0, repr(getattr(record, field)))
+
+    import requests
+    # todo нужна валидация полей, введенных юзером
+    recordNumber = entries['key'].get()
+
+    target = 'http://khatangatao.com/api/entry/' + recordNumber
+    headers = {"Authorization": "Bearer 56a4f78b7f4a6990d3ae6c223a73249b8765c0c1"}
+
+    r = requests.get(target, headers=headers)
+    print(r.status_code)
+    print(r.json())
+    if r.status_code == 200:
         for field in fieldnames:
+            print(field)
+            # entries[field] = r.json().get(field)
             entries[field].delete(0, END)
-            entries[field].insert(0, repr(getattr(record, field)))
+            entries[field].insert(0, r.json().get(field, ''))
+            print(entries[field].get())
+    elif r.status_code == 404:
+        showerror(title='Error', message='Такой статьи нет!')
+
 
 
 def updateRecord():
-    key = entries['key'].get()
-    if key in db:
-        record = db[key]                                                # update existing record
-    else:
-        from person import Person                                       # make/store new one for key
-        record = Person(name='?', age='?')                              # eval: strings must be quoted
-    for field in fieldnames:
-        setattr(record, field, eval(entries[field].get()))
-    db[key] = record
+    # key = entries['key'].get()
+    # if key in db:
+    #     record = db[key]                                                # update existing record
+    # else:
+    #     from person import Person                                       # make/store new one for key
+    #     record = Person(name='?', age='?')                              # eval: strings must be quoted
+    # for field in fieldnames:
+    #     setattr(record, field, eval(entries[field].get()))
+    # db[key] = record
+    import requests
+    import json
+    # recordNumber = entries['key'].get()
+    data = {}
 
+    target = 'http://khatangatao.com/api/entry/'
+    headers = {"Authorization": "Bearer 56a4f78b7f4a6990d3ae6c223a73249b8765c0c1",
+               "Content-Type": "application/json"}
+    for field in fieldnames:
+        data[field] = entries[field].get()
+
+    data = json.dumps(data)
+    print(data)
+
+    r = requests.post(url=target, data=data, headers=headers)
+    print(r.status_code)
 
 def checkAvaibility():
+    # todo распараллелить с основным потоком
     import subprocess
-    reply = subprocess.run(['ping', '-c', '3', '-n', '8.8.8.8'],
+    reply = subprocess.run(['ping', '-c', '3', '-n', 'khatangatao.com'],
                            stdout=subprocess.PIPE,
                            stdin=subprocess.PIPE,
                            )
@@ -69,7 +106,5 @@ def checkAvaibility():
         showinfo(title='popup', message='Не работает!')
 
 
-db = shelve.open(shelvename)
 window = makeWidgets()
 window.mainloop()
-db.close()                                                              # back here after quit or window close
